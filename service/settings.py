78023894b6f725e5e971443e50a944942415e794
orel8654 <88678826+orel8654@users.noreply.github.com>
2022-07-10 21:34:19 +0700

    vercelfix1

--- service/settings.py
+++ service/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
