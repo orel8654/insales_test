f4ca93d777f07c0461d3c2779b1b7e3e6bbe08ab
orel8654 <88678826+orel8654@users.noreply.github.com>
2022-07-29 21:32:11 +0700

    Good

--- restapi/views.py
+++ restapi/views.py
@@ -28,7 +28,7 @@ class CreateUserAPI(APIView):
         #     EMAIL_HOST_USER,
         #     ['egorao@mail.ru'],
         # )
-        return Response({'status': 'OK', 'some': 'Cool!'})
+        return Response({'status': 'OK', 'some': 'Beautiful!'})
 
 def index(request):
     return render(request)
